8aca231222b5abbe36504eefd6e3f615acdaa3c0
Chad Hamdan <chad.hamdan4@gmail.com>
2022-03-17 23:54:14 -0400

    Converted Nemi & Rago to Slash Commands

---
+++ js/commands/nemi.js
@@ -0,0 +1,36 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const Discord = require("discord.js");
+const constants = require("../constants");
+const _ = require('lodash');
+const rp = require('request-promise');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('nemi')
+    .setDescription(`Displays current Nemi Forest Map`),
+  async execute(interaction) {
+    await rp('https://www.reddit.com/r/nemiforest/new.json?limit=1').then(function (json) {
+      const data = JSON.parse(json);
+
+      let title = data.data.children[0].data.title;
+      let imageURL = data.data.children[0].data.url_overridden_by_dest;
+
+      let embed = new Discord.MessageEmbed()
+        .setColor(constants.embedColor)
+        .setTitle(`NemiForest`)
+        .setImage(imageURL)
+        .setDescription(title)
+        .setTimestamp()
+        .setFooter('ChadTek', 'https://raw.githubusercontent.com/Chad414/RSClan-Bot/main/img/icon.png');
+
+      interaction.reply({ embeds: [embed] });
+
+    }).catch(function (err) {
+      interaction.reply({ embeds: [constants.portablesError] });
+      constants.logError({
+        name: "Reddit: Nemi",
+        message: "Nemi Command Error" + err,
+      });
+    });
+  }
+};
---
+++ js/commands/rago.js
@@ -0,0 +1,39 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const Discord = require("discord.js");
+const constants = require("../constants");
+const _ = require('lodash');
+const rp = require('request-promise');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('rago')
+    .setDescription(`Displays current and next Vorago rotation`),
+  async execute(interaction) {
+
+    let firstRotationDate = new Date('June 2, 2021 00:00:00 GMT+0:00');
+    let currentDate = new Date();
+
+    let timeDifference = currentDate.getTime() - firstRotationDate.getTime();
+    let daysDifference = Math.floor(timeDifference / (1000 * 3600 * 24));
+
+    let numberOfRotations = Math.floor(daysDifference / 7);
+
+    let currentRotation = numberOfRotations % 6;
+
+    let daysUntilRotation = 7 - (daysDifference % 7);
+
+    let embed = new Discord.MessageEmbed()
+      .setColor(constants.embedColor)
+      .setTitle(`Vorago's Current Rotation`)
+      .setDescription(`Days Until Next Rotation: ${daysUntilRotation}`)
+      .setThumbnail('https://raw.githubusercontent.com/Chad414/RSClan-Bot/main/img/rago.png')
+      .addFields(
+        { name: 'Vorago Current', value: `${constants.voragoRotations[currentRotation]}` },
+        { name: 'Vorago Next', value: `${constants.voragoRotations[(currentRotation + 1) % 6]}` },
+      )
+      .setTimestamp()
+      .setFooter('ChadTek', 'https://raw.githubusercontent.com/Chad414/RSClan-Bot/main/img/icon.png');
+
+    interaction.reply({ embeds: [embed] });
+  }
+};
